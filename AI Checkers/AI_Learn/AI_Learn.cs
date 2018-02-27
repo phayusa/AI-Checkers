a7deaa89d430865170a0f096426b3220ee5506c3
Unknown <sokomo873@free.fr>
2018-02-27 11:06:23 +0100

    Add of AI Learn

---
+++ AI Checkers/AI_Learn/AI_Learn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace AICheckers
+{
+    class AI_Learn : IAI
+    {
+        CheckerColour colour;
+
+        public CheckerColour Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+
+        public Move Process(Square[,] Board)
+        {
+            List<Move> moves = new List<Move>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (Board[i, j].Colour == Colour)
+                    {
+                        moves.AddRange(Utils.GetOpenSquares(Board, new Point(j, i)));
+                    }
+                }
+            }
+
+            return moves[(new Random()).Next(moves.Count)];
+        }
+    }
+}
